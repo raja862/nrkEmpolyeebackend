c88e3fc67ab52fff4379d574f5a3951439b07a29
N Rajakumaran <108915966+raja862@users.noreply.github.com>
2023-12-22 11:21:55 +0530

    Update employees.model.js

--- models/employees.model.js
+++ models/employees.model.js
@@ -5,31 +5,20 @@ const employeeSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
-        trim: true
+        
     },
     email: {
         type: String,
-        unique: true,
         required: true,
-        trim: true
+    
     },
     address: {
         type: String,
         required: true,
        
     },
-    designation: {
-        type: String,
-        
-    },
-    mobileNumber: {
-        type: String,
-        unique:true,
-       
-    },
-    bloodGroup: {
-        type: String
-    }
+    
+    
 });
 
 //Model creation
